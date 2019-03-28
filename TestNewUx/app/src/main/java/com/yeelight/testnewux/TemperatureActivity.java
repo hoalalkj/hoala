669b918fe4e6f181afdf5d43d0d71054149e3852
jordan-ooka <31424274+jordan-ooka@users.noreply.github.com>
2019-03-27 19:15:35 -1000

    Added Temperature Data Scraping
    Needs To Be Tested

--- TestNewUx/app/src/main/java/com/yeelight/testnewux/TemperatureActivity.java
+++ TestNewUx/app/src/main/java/com/yeelight/testnewux/TemperatureActivity.java
@@ -2,6 +2,11 @@ package com.yeelight.testnewux;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
 
 public class TemperatureActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class TemperatureActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperature);
+        String TemperatureString;
+        float temp_k;
+        double temp_f;
+        org.jsoup.nodes.Document document = Jsoup.parse("http://api.openweathermap.org/data/2.5/weather?q=Honolulu,usa&APPID=cbf0ac4717c33ead47cd80623c897eb3");
+        try{
+            JSONObject jsonObj = new JSONObject (document.select("td.line-content").first().text());
+            JSONObject main1 = (JSONObject) jsonObj.get("main");
+            temp_k = (float) main1.get("temp");
+            temp_f = (temp_k - 273.15) * 1.8 + 32;
+            TemperatureString = Double.toString(temp_f);
+            ((TextView) findViewById(R.id.textView)).setText(TemperatureString);
+        }
+        catch(JSONException e){
+            System.out.println("JSONException Error");
+        }
     }
 }
