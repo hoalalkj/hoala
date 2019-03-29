89c071ad098e1d4770573dc1f1db600fafad37f6
jordan-ooka <31424274+jordan-ooka@users.noreply.github.com>
2019-03-28 14:49:20 -1000

    Actually has Data being Scrapped from Weather Website
    Needs to figure out CT

--- TestNewUx/app/src/main/java/com/yeelight/testnewux/TemperatureActivity.java
+++ TestNewUx/app/src/main/java/com/yeelight/testnewux/TemperatureActivity.java
@@ -2,32 +2,143 @@ package com.yeelight.testnewux;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.jsoup.Jsoup;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.io.InputStreamReader;
+import android.os.AsyncTask;
+import java.net.URL;
+import java.net.Socket;
+
+import java.io.IOException;
 
 public class TemperatureActivity extends AppCompatActivity {
+    private String TAG = "Control";
+    private static final String CMD_CT = "{\"id\":%id,\"method\":\"set_ct_abx\",\"params\":[%value, \"smooth\", 500]}\r\n";
+
+    private int mCmdId;
+    private Socket mSocket;
+    private String mBulbIP;
+    private int mBulbPort;
+    private BufferedOutputStream mBos;
+    private BufferedReader mReader;
+
+
+    TextView text = null;
+    double temp_k;
+    double temp_f;
+    String TemperatureString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperature);
-        String TemperatureString;
-        float temp_k;
-        double temp_f;
-        org.jsoup.nodes.Document document = Jsoup.parse("http://api.openweathermap.org/data/2.5/weather?q=Honolulu,usa&APPID=cbf0ac4717c33ead47cd80623c897eb3");
-        try{
-            JSONObject jsonObj = new JSONObject (document.select("td.line-content").first().text());
-            JSONObject main1 = (JSONObject) jsonObj.get("main");
-            temp_k = (float) main1.get("temp");
-            temp_f = (temp_k - 273.15) * 1.8 + 32;
-            TemperatureString = Double.toString(temp_f);
-            ((TextView) findViewById(R.id.textView)).setText(TemperatureString);
+        mBulbIP = getIntent().getStringExtra("ip");
+        mBulbPort = Integer.parseInt(getIntent().getStringExtra("port"));
+        new TemperatureGetter().execute();
+    }
+
+    private class TemperatureGetter extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                InputStream is = new URL("http://api.openweathermap.org/data/2.5/weather?q=Honolulu,usa&APPID=cbf0ac4717c33ead47cd80623c897eb3").openStream();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+                StringBuilder sb = new StringBuilder();
+                int cp;
+                while ((cp = rd.read()) != -1) {
+                    sb.append((char) cp);
+                }
+                String jsonText = sb.toString();
+                JSONObject jsonObj = new JSONObject(jsonText);
+                JSONObject main1 = (JSONObject) jsonObj.get("main");
+                temp_k = (double) main1.get("temp");
+                temp_f = (temp_k - 273.15) * 1.8 + 32;
+                TemperatureString = Integer.toString((int) temp_f);
+                connect();
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
         }
-        catch(JSONException e){
-            System.out.println("JSONException Error");
+
+        @Override
+        protected void onPostExecute(Void result) {
+            text = (TextView) findViewById(R.id.textView);
+            text.setText(TemperatureString);
+            if((int) temp_f < 65){
+                write(parseCTCmd(3000));
+
+            }
+            else{
+                System.out.println("HELLO");
+                write(parseCTCmd(3000));
+            }
+
+        }
+    }
+
+    private boolean cmd_run = true;
+
+    private void connect() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    cmd_run = true;
+                    mSocket = new Socket(mBulbIP, mBulbPort);
+                    mSocket.setKeepAlive(true);
+                    mBos = new BufferedOutputStream(mSocket.getOutputStream());
+                    mReader = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
+                    while (cmd_run) {
+                        try {
+                            String value = mReader.readLine();
+                            Log.d(TAG, "value = " + value);
+                        } catch (Exception e) {
+
+                        }
+
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            cmd_run = false;
+            if (mSocket != null)
+                mSocket.close();
+        } catch (Exception e) {
+
+        }
+    }
+    private String parseCTCmd(int ct){
+        return CMD_CT.replace("%id",String.valueOf(++mCmdId)).replace("%value",String.valueOf(ct));
+    }
+
+    private void write(String cmd){
+        if (mBos != null && mSocket.isConnected()){
+            try {
+                mBos.write(cmd.getBytes());
+                mBos.flush();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        } else {
+            Log.d(TAG,"mBos = null or mSocket is closed");
         }
     }
 }
