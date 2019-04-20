be121d3e22b70ae525af64b77583c365a08d65f1
Leisha Soberano-Keawemauhili <42786840+leishask@users.noreply.github.com>
2019-04-19 17:25:21 -1000

    Implemented Mood feature
    - Created a new feature based on user's mood
    - Four moods: Happy, Sad, Sleepy, Party
    - When the user clicks on a mood, the yeelight light bulb is manipulated in different ways.
    - For Happy: color temp and brightness is changed
    - For Sad: color temp and brightness is changed
    - For Sleepy: color temp and brightness is changed until user clicks dialog box to turn bulb off
    - For Party: color flow of temperature is changed until user clicks dialog box to stop

--- TestNewUx/app/src/main/java/com/yeelight/testnewux/MoodActivity.java
+++ TestNewUx/app/src/main/java/com/yeelight/testnewux/MoodActivity.java
@@ -1,6 +1,7 @@
 package com.yeelight.testnewux;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
@@ -19,8 +21,11 @@ public class MoodActivity extends AppCompatActivity {
 
     private static final int MSG_CONNECT_SUCCESS = 0;
     private static final int MSG_CONNECT_FAILURE = 1;
-    private static final String CMD_BRIGHTNESS = "{\"id\":%id,\"method\":\"set_bright\",\"params\":[100, \"smooth\", %duration]}\r\n";
-    private static final String CMD_PARTY = "{\"id\":%id,\"method\":\"start_cf\",\"params\":[4, 2, %duration, %mode, %value, %brightness]}\r\n";
+    private static final String CMD_BRIGHTNESS = "{\"id\":%id,\"method\":\"set_bright\",\"params\":[%value, \"smooth\", %duration]}\r\n";
+    private static final String CMD_STOP2 = "{\"id\":%id,\"method\":\"set_power\",\"params\":[\"off\", \"smooth\", 1000]}\r\n";
+    private static final String CMD_CT = "{\"id\":%id,\"method\":\"set_ct_abx\",\"params\":[%value, \"smooth\", 500]}\r\n";
+    private static final String CMD_PARTY = "{\"id\":%id,\"method\":\"start_cf\",\"params\":[0, 0, \"1000, 2, 2700, 100, 1000, 2, 6500, 100\"]}\r\n";
+    private static final String CMD_STOP = "{\"id\":%id,\"method\":\"stop_cf\",\"params\":[]}\r\n";
 
     private int mCmdId;
     private Socket mSocket;
@@ -65,7 +70,8 @@ public class MoodActivity extends AppCompatActivity {
         ((View) happyMood).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                write(parseBrightnessCmd(60000));
+                write(parseCTCmd(5000));
+                write(parseBrightnessCmd(80,10000));
             }
         });
 
@@ -73,7 +79,8 @@ public class MoodActivity extends AppCompatActivity {
         ((View) sadMood).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                write(parseCTCmd(3500));
+                write(parseBrightnessCmd(90,20000));
             }
         });
 
@@ -81,7 +88,9 @@ public class MoodActivity extends AppCompatActivity {
         ((View) sleepyMood).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                write(parseCTCmd(2700));
+                write(parseBrightnessCmd(1,10000));
+                customDialog("Stop Sleep Mood", "Click 'Stop' to turn off light bulb.", "cancelMethod2");
             }
         });
 
@@ -89,10 +98,48 @@ public class MoodActivity extends AppCompatActivity {
         ((View) partyMood).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                write(parsePartyCmd(1000, 2, 2700, 100));
+                write(parsePartyCmd());
+                customDialog("Stop Party Mood", "Click 'Stop' to stop light bulb.", "cancelMethod");
             }
         });
         connect();
+    }
+
+    private void cancelMethod(){
+        write(parsePartyStopCmd());
+        Log.d(TAG, "Party Mood Stopped");
+        toastMessage("Party Mood Stopped");
+    }
+
+    private void cancelMethod2(){
+        write(parseSleepStopCmd());
+        Log.d(TAG, "Sleepy Mood Stopped");
+        toastMessage("Sleepy Mood Stopped");
+    }
+
+    public void customDialog(String title, String message, final String cancelMethod){
+        final android.support.v7.app.AlertDialog.Builder builderSingle = new android.support.v7.app.AlertDialog.Builder(this);
+        builderSingle.setTitle(title);
+        builderSingle.setMessage(message);
+
+        builderSingle.setNegativeButton(
+                "Stop",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d(TAG, "onClick: Cancel Called.");
+                        if(cancelMethod.equals("cancelMethod")) {
+                            cancelMethod();
+                        } else {
+                            cancelMethod2();
+                        }
+                    }
+                });
+        builderSingle.show();
+    }
+
+    public void toastMessage(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 
     private boolean cmd_run = true;
@@ -136,11 +183,22 @@ public class MoodActivity extends AppCompatActivity {
         }
 
     }
-    private String parseBrightnessCmd(int duration){
-        return CMD_BRIGHTNESS.replace("%id",String.valueOf(++mCmdId)).replace("%duration",String.valueOf(duration));
+
+    private String parseCTCmd(int ct){
+        return CMD_CT.replace("%id",String.valueOf(++mCmdId)).replace("%value", String.valueOf(ct));
     }
-    private String parsePartyCmd(int duration, int mode, int value, int brightness){
-        return CMD_PARTY.replace("%id",String.valueOf(++mCmdId)).replace("%duration",String.valueOf(duration));
+    private String parseBrightnessCmd(int brightness, int duration){
+        return CMD_BRIGHTNESS.replace("%id",String.valueOf(++mCmdId)).replace("%value",String.valueOf(brightness)).replace("%duration",String.valueOf(duration));
+    }
+    private String parseSleepStopCmd(){
+        return CMD_STOP2.replace("%id",String.valueOf(++mCmdId));
+    }
+    private String parsePartyCmd(){
+        return CMD_PARTY.replace("%id",String.valueOf(++mCmdId));
+    }
+
+    private String parsePartyStopCmd(){
+        return CMD_STOP.replace("%id",String.valueOf(++mCmdId));
     }
 
     private void write(String cmd){
